cca5d041c8024f48dcdc87bba56fac159c904fda
mariem-hammami <hamamimariam6@gmail.com>
2024-12-24 11:18:25 +0100

    Ajoutez des fichiers projet.

---
+++ calculatrice2/calculatrice2.Designer.vb
@@ -0,0 +1,275 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class calculatrice2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        txt_afficher = New TextBox()
+        btn_0 = New Button()
+        btn_1 = New Button()
+        btn_2 = New Button()
+        btn_3 = New Button()
+        btn_4 = New Button()
+        btn_5 = New Button()
+        btn_6 = New Button()
+        btn_7 = New Button()
+        btn_8 = New Button()
+        btn_9 = New Button()
+        btn_c = New Button()
+        btn_equal = New Button()
+        btn_plus = New Button()
+        btn_minus = New Button()
+        btn_mult = New Button()
+        btn_div = New Button()
+        SuspendLayout()
+        ' 
+        ' txt_afficher
+        ' 
+        txt_afficher.Location = New Point(346, 37)
+        txt_afficher.Name = "txt_afficher"
+        txt_afficher.Size = New Size(125, 27)
+        txt_afficher.TabIndex = 0
+        ' 
+        ' btn_0
+        ' 
+        btn_0.BackColor = Color.Turquoise
+        btn_0.Font = New Font("Segoe UI", 10.8F, FontStyle.Bold)
+        btn_0.ForeColor = SystemColors.InactiveCaptionText
+        btn_0.Location = New Point(137, 122)
+        btn_0.Name = "btn_0"
+        btn_0.Size = New Size(105, 63)
+        btn_0.TabIndex = 1
+        btn_0.Text = "0"
+        btn_0.UseVisualStyleBackColor = False
+        ' 
+        ' btn_1
+        ' 
+        btn_1.BackColor = Color.Turquoise
+        btn_1.Font = New Font("Segoe UI", 10.8F, FontStyle.Bold)
+        btn_1.Location = New Point(296, 122)
+        btn_1.Name = "btn_1"
+        btn_1.Size = New Size(94, 54)
+        btn_1.TabIndex = 2
+        btn_1.Text = "1"
+        btn_1.UseVisualStyleBackColor = False
+        ' 
+        ' btn_2
+        ' 
+        btn_2.BackColor = Color.Turquoise
+        btn_2.Font = New Font("Segoe UI", 10.8F, FontStyle.Bold)
+        btn_2.Location = New Point(438, 134)
+        btn_2.Name = "btn_2"
+        btn_2.Size = New Size(94, 46)
+        btn_2.TabIndex = 3
+        btn_2.Text = "2"
+        btn_2.UseVisualStyleBackColor = False
+        ' 
+        ' btn_3
+        ' 
+        btn_3.BackColor = Color.Turquoise
+        btn_3.Font = New Font("Segoe UI", 10.8F, FontStyle.Bold)
+        btn_3.Location = New Point(566, 134)
+        btn_3.Name = "btn_3"
+        btn_3.Size = New Size(94, 42)
+        btn_3.TabIndex = 4
+        btn_3.Text = "3"
+        btn_3.UseVisualStyleBackColor = False
+        ' 
+        ' btn_4
+        ' 
+        btn_4.BackColor = Color.Turquoise
+        btn_4.Font = New Font("Segoe UI", 10.8F, FontStyle.Bold)
+        btn_4.Location = New Point(137, 191)
+        btn_4.Name = "btn_4"
+        btn_4.Size = New Size(105, 52)
+        btn_4.TabIndex = 5
+        btn_4.Text = "4"
+        btn_4.UseVisualStyleBackColor = False
+        ' 
+        ' btn_5
+        ' 
+        btn_5.BackColor = Color.Turquoise
+        btn_5.Font = New Font("Segoe UI", 10.8F, FontStyle.Bold)
+        btn_5.Location = New Point(296, 190)
+        btn_5.Name = "btn_5"
+        btn_5.Size = New Size(94, 46)
+        btn_5.TabIndex = 6
+        btn_5.Text = "5"
+        btn_5.UseVisualStyleBackColor = False
+        ' 
+        ' btn_6
+        ' 
+        btn_6.BackColor = Color.Turquoise
+        btn_6.Font = New Font("Segoe UI", 10.8F, FontStyle.Bold)
+        btn_6.Location = New Point(438, 194)
+        btn_6.Name = "btn_6"
+        btn_6.Size = New Size(94, 46)
+        btn_6.TabIndex = 7
+        btn_6.Text = "6"
+        btn_6.UseVisualStyleBackColor = False
+        ' 
+        ' btn_7
+        ' 
+        btn_7.BackColor = Color.Turquoise
+        btn_7.Font = New Font("Segoe UI", 10.8F, FontStyle.Bold)
+        btn_7.Location = New Point(566, 190)
+        btn_7.Name = "btn_7"
+        btn_7.Size = New Size(94, 46)
+        btn_7.TabIndex = 8
+        btn_7.Text = "7"
+        btn_7.UseVisualStyleBackColor = False
+        ' 
+        ' btn_8
+        ' 
+        btn_8.BackColor = Color.Turquoise
+        btn_8.Font = New Font("Segoe UI", 10.8F, FontStyle.Bold)
+        btn_8.Location = New Point(137, 251)
+        btn_8.Name = "btn_8"
+        btn_8.Size = New Size(105, 52)
+        btn_8.TabIndex = 9
+        btn_8.Text = "8"
+        btn_8.UseVisualStyleBackColor = False
+        ' 
+        ' btn_9
+        ' 
+        btn_9.BackColor = Color.Turquoise
+        btn_9.Font = New Font("Segoe UI", 10.8F, FontStyle.Bold)
+        btn_9.Location = New Point(296, 251)
+        btn_9.Name = "btn_9"
+        btn_9.Size = New Size(94, 42)
+        btn_9.TabIndex = 10
+        btn_9.Text = "9"
+        btn_9.UseVisualStyleBackColor = False
+        ' 
+        ' btn_c
+        ' 
+        btn_c.BackColor = Color.Turquoise
+        btn_c.Font = New Font("Segoe UI", 10.8F, FontStyle.Bold)
+        btn_c.Location = New Point(438, 249)
+        btn_c.Name = "btn_c"
+        btn_c.Size = New Size(94, 45)
+        btn_c.TabIndex = 11
+        btn_c.Text = "c"
+        btn_c.UseVisualStyleBackColor = False
+        ' 
+        ' btn_equal
+        ' 
+        btn_equal.BackColor = Color.Turquoise
+        btn_equal.Font = New Font("Segoe UI", 10.8F, FontStyle.Bold)
+        btn_equal.Location = New Point(566, 251)
+        btn_equal.Name = "btn_equal"
+        btn_equal.Size = New Size(94, 52)
+        btn_equal.TabIndex = 12
+        btn_equal.Text = "="
+        btn_equal.UseVisualStyleBackColor = False
+        ' 
+        ' btn_plus
+        ' 
+        btn_plus.BackColor = Color.Turquoise
+        btn_plus.Font = New Font("Segoe UI", 10.8F, FontStyle.Bold)
+        btn_plus.Location = New Point(137, 309)
+        btn_plus.Name = "btn_plus"
+        btn_plus.Size = New Size(105, 43)
+        btn_plus.TabIndex = 13
+        btn_plus.Text = "+"
+        btn_plus.UseVisualStyleBackColor = False
+        ' 
+        ' btn_minus
+        ' 
+        btn_minus.BackColor = Color.Turquoise
+        btn_minus.Font = New Font("Segoe UI", 10.8F, FontStyle.Bold)
+        btn_minus.Location = New Point(296, 313)
+        btn_minus.Name = "btn_minus"
+        btn_minus.Size = New Size(94, 39)
+        btn_minus.TabIndex = 14
+        btn_minus.Text = "-"
+        btn_minus.UseVisualStyleBackColor = False
+        ' 
+        ' btn_mult
+        ' 
+        btn_mult.BackColor = Color.Turquoise
+        btn_mult.Font = New Font("Segoe UI", 10.8F, FontStyle.Bold)
+        btn_mult.Location = New Point(438, 309)
+        btn_mult.Name = "btn_mult"
+        btn_mult.Size = New Size(94, 43)
+        btn_mult.TabIndex = 15
+        btn_mult.Text = "*"
+        btn_mult.UseVisualStyleBackColor = False
+        ' 
+        ' btn_div
+        ' 
+        btn_div.BackColor = Color.Turquoise
+        btn_div.Font = New Font("Segoe UI", 10.8F, FontStyle.Bold)
+        btn_div.Location = New Point(566, 313)
+        btn_div.Name = "btn_div"
+        btn_div.Size = New Size(94, 39)
+        btn_div.TabIndex = 16
+        btn_div.Text = "/"
+        btn_div.UseVisualStyleBackColor = False
+        ' 
+        ' calculatrice2
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.DeepPink
+        ClientSize = New Size(800, 450)
+        Controls.Add(btn_div)
+        Controls.Add(btn_mult)
+        Controls.Add(btn_minus)
+        Controls.Add(btn_plus)
+        Controls.Add(btn_equal)
+        Controls.Add(btn_c)
+        Controls.Add(btn_9)
+        Controls.Add(btn_8)
+        Controls.Add(btn_7)
+        Controls.Add(btn_6)
+        Controls.Add(btn_5)
+        Controls.Add(btn_4)
+        Controls.Add(btn_3)
+        Controls.Add(btn_2)
+        Controls.Add(btn_1)
+        Controls.Add(btn_0)
+        Controls.Add(txt_afficher)
+        Name = "calculatrice2"
+        Text = "calculatrice2"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents txt_afficher As TextBox
+    Friend WithEvents btn_0 As Button
+    Friend WithEvents btn_1 As Button
+    Friend WithEvents btn_2 As Button
+    Friend WithEvents btn_3 As Button
+    Friend WithEvents btn_4 As Button
+    Friend WithEvents btn_5 As Button
+    Friend WithEvents btn_6 As Button
+    Friend WithEvents btn_7 As Button
+    Friend WithEvents btn_8 As Button
+    Friend WithEvents btn_9 As Button
+    Friend WithEvents btn_c As Button
+    Friend WithEvents btn_equal As Button
+    Friend WithEvents btn_plus As Button
+    Friend WithEvents btn_minus As Button
+    Friend WithEvents btn_mult As Button
+    Friend WithEvents btn_div As Button
+
+End Class
